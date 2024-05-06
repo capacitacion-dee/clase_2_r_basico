f1e8df3293d4b6027f629cd1ac91b66aa97cb24a
Loreto Cristina Gallardo Villegas <lcgallardov@ine.gob.cl>
2024-05-06 08:30:19 -0400

    ajuste

--- code/tarea_2.R
+++ code/tarea_2.R
@@ -20,7 +20,8 @@ superficie %>%
 superficie %>% 
   select(starts_with("superficie"))
 
-superficie %>% mutate(mes_nombre = case_when(mes_num == 1 ~ "Enero",
+superficie %>% 
+  mutate(mes_nombre = case_when(mes_num == 1 ~ "Enero",
                                              mes_num == 2 ~ "Febrero",
                                              mes_num == 3 ~ "Marzo",
                                              mes_num == 4 ~ "Abril",
@@ -33,8 +34,11 @@ superficie %>% mutate(mes_nombre = case_when(mes_num == 1 ~ "Enero",
                                              mes_num == 11 ~ "Noviembre",
                                              mes_num == 12 ~ "Diciembre"))
 
+superficie %>% group_by(anio) %>% summarise(superficie_promedio = mean(superficie_total))
 
-superficie %>% 
-  mutate(tramo_superficie = if_else(superficie_total >= 1200000, "grande", "pequeña")) %>% 
-  group_by(tramo_superficie) %>% 
-  summarise(superficie_promedio = mean(superficie_total))
+superficie %>% mutate(tramo_superficie = case_when(superficie_total >= 1500000 ~ "grande",
+                                                   superficie_total >= 1200000 ~ "mediana",
+                                                   TRUE ~ "pequeña")) %>% 
+  filter(tramo_superficie=="grande" & anio == 2019) %>% 
+  mutate(vivienda_mayor = if_else(superficie_vivienda > superficie_no_vivienda, TRUE, FALSE)) %>% 
+  mutate(superficie_hect = superficie_total/10000)
